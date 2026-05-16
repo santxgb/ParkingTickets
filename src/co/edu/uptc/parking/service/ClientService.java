255a37c60e6e7c191b221fa22d72641a5840381c
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-05-15 21:23:56 -0500

    Agregué la clase de ClientService completa, con todos los métodos respectivos de dicha clase y pues ya toca es aplicar casi lo mismo en las otras dos de la capa de service

--- src/co/edu/uptc/parking/service/ClientService.java
+++ src/co/edu/uptc/parking/service/ClientService.java
@@ -1,5 +1,53 @@
 package co.edu.uptc.parking.service;
 
-public class ClientService {
+import java.util.List;
 
+import co.edu.uptc.parking.domain.Client;
+import co.edu.uptc.parking.repository.ClientRepository;
+
+public class ClientService {
+	
+	private ClientRepository clientRepository;
+
+	/**
+	 * Crea una nueva instancia de ClientService.
+	 *
+	 * @param clientRepository Parámetro que determina
+	 */
+	public ClientService() {
+		super();
+		this.clientRepository = new ClientRepository();
+	}
+	
+	public boolean addClient(Client client) {
+		if(clientRepository.existsById(client.getClientId())) {
+			return false;
+		}
+		clientRepository.add(client);
+		return true;
+	}
+	
+	public Client findById(String clientId) {
+		return clientRepository.findById(clientId);
+	}
+	
+	public List<Client> findAll(){
+		return clientRepository.findAll();
+	}
+	
+	public boolean updateClient(Client client) {
+		if(!clientRepository.existsById(client.getClientId())) {
+			return false;
+		}
+		clientRepository.add(client);
+		return true;
+	}
+	
+	public boolean deleteClient(String clientId) {
+		return clientRepository.deleteById(clientId);
+	}
+	
+	public boolean existsById(String clientId) {
+		return clientRepository.existsById(clientId);
+	}
 }
