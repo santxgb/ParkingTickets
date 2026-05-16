a1cfcecb2c27e9fcfa8fa20f56272ba22f252f62
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-05-15 22:45:42 -0500

    Agregué la clase de TicketService pero nos hace falta el método de calcularValorTotal, sino que aún no sé muy bien cómo hacerlo

--- src/co/edu/uptc/parking/service/TicketService.java
+++ src/co/edu/uptc/parking/service/TicketService.java
@@ -1,5 +1,47 @@
 package co.edu.uptc.parking.service;
 
-public class TicketService {
+import java.util.List;
 
+import co.edu.uptc.parking.domain.Ticket;
+import co.edu.uptc.parking.repository.TicketRepository;
+
+public class TicketService {
+	
+	private TicketRepository ticketRepository;
+
+	/**
+	 * Crea una nueva instancia de TicketService.
+	 *
+	 * @param ticketRepository Parámetro que determina
+	 */
+	public TicketService() {
+		super();
+		this.ticketRepository = new TicketRepository();
+	}
+	
+	public boolean addTicket(Ticket ticket) {
+		if(ticketRepository.existsById(ticket.getTicketId())) {
+			return false;
+		}
+		ticketRepository.add(ticket);
+		return true;
+	}
+	
+	public Ticket findById(String ticketId) {
+		return ticketRepository.findById(ticketId);
+	}
+	
+	public List<Ticket> findAll(){
+		return ticketRepository.findAll();
+	}
+	
+	public boolean deleteTicket(String ticketId) {
+		return ticketRepository.deleteById(ticketId);
+	}
+	
+	public boolean existsById(String ticketId) {
+		return ticketRepository.existsById(ticketId);
+	}
+	
+	
 }
